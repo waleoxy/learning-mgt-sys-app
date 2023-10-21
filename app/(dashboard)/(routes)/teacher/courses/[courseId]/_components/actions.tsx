11c6c369b8e39e240b46492bdb3e97fdc7b3efb3
Olawale Otubu <waleoxy@gmail.com>
2023-10-21 22:19:29 +0100

    feat:course publish func impl

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/actions.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import ConfirmModal from "@/components/modals/confirm-modals";
+import { Button } from "@/components/ui/button";
+import axios from "axios";
+import { Trash } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import toast from "react-hot-toast";
+
+interface ActionsProps {
+  disabled: boolean;
+  courseId: string;
+  isPublished: boolean;
+}
+
+const Actions: React.FC<ActionsProps> = ({
+  courseId,
+  disabled,
+  isPublished,
+}) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+  const onClick = async () => {
+    console.log(isPublished);
+
+    try {
+      setIsLoading(true);
+      await axios.patch(`/api/courses/${courseId}?&publish=${isPublished}`);
+      toast.success("Course published");
+      router.refresh();
+    } catch (error) {
+      toast.error("Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const onDelete = async () => {
+    try {
+      setIsLoading(false);
+
+      await axios.delete(`/api/courses/${courseId}`);
+      toast.success("Course deleted");
+      router.refresh();
+      router.push(`/teacher/courses`);
+    } catch (error) {
+      toast.error("Something went wrong");
+    } finally {
+      setIsLoading(true);
+    }
+  };
+
+  return (
+    <div className="flex items-center gap-x-2">
+      <Button
+        onClick={onClick}
+        disabled={disabled || isLoading}
+        variant="outline"
+        size="sm">
+        {isPublished ? "Unpublish" : "Publish"}
+      </Button>
+      <ConfirmModal onConfirm={onDelete}>
+        <Button size="sm" disabled={isLoading}>
+          <Trash className="h-4 w-4" />
+        </Button>
+      </ConfirmModal>
+    </div>
+  );
+};
+
+export default Actions;
