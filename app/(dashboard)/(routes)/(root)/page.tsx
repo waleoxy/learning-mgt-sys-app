e604984800c6e86d49181e9d88b68804170223b8
Olawale Otubu <waleoxy@gmail.com>
2023-11-06 16:03:51 +0100

    dashboard setup

---
+++ app/(dashboard)/(routes)/(root)/page.tsx
@@ -0,0 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
+interface HomeProps {}
+
+const Dashboard: React.FC<HomeProps> = ({}) => {
+  return <div className="p-6"></div>;
+};
+
+export default Home;
--- app/(dashboard)/(routes)/page.tsx
+++
@@ -1,9 +0,0 @@
-import { UserButton } from "@clerk/nextjs";
-
-interface HomeProps {}
-
-const Home: React.FC<HomeProps> = ({}) => {
-  return <div className="text-3xl p-6">This is the home page</div>;
-};
-
-export default Home;
